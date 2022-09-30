7e79ce70a6ee426a362a4b5657e5fbdfec7de041
pabloarshid <umarsa.birvancity@gmail.com>
2022-09-29 17:03:25 -0700

    added template for blog_index page

--- personal_portfolio/personal_portfolio/urls.py
+++ personal_portfolio/personal_portfolio/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("blog/", include("blog.urls")),
     # path('',include('hello_world.urls')),
 ]
--- personal_portfolio/projects/urls.py
+++ personal_portfolio/projects/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.project_index, name="project_index"),
     path("<int:pk>/", views.project_detail, name="project_detail"),
-
+    
 ]
